072d80c777da5e7a0b6cc686cb4a70216b03cfbd
mocar <mocar002@gmail.com>
2023-12-03 23:45:36 -0600

    Added JavaScript to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,5 @@
 var topic = "HTML";
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
 if (topic === 'HTML') {
   console.log("Let's study HTML!");
@@ -11,5 +12,29 @@ if (topic === 'HTML') {
 } else {
   console.log('Please try again!');
 }
-*/
+
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
+ }
+ 
+ function selectTopic() {
+  if (randomTopic === 'HTML') {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === 'CSS') {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === 'Git') {
+    console.log("Let's study Git!");
+  } else if (randomTopic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log('Please try again!');
+  }
+ }
+ 
+ console.log('Here are the topics we learned through Prework:');
+ listTopics();
+ console.log('Which topic should we study first?');
+ selectTopic();
 
